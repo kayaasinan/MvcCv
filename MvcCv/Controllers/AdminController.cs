d25f6626374c45e20a010660f159af0c87ca99ac
Sinan Kaya <skaya.sinan@outlook.com>
2025-07-16 17:20:34 +0300

    _

---
+++ MvcCv/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        AdminRepository repo=new AdminRepository();
+        public ActionResult Index()
+        {
+            var values=repo.List();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(TblAdmins admin)
+        {
+            repo.TAdd(admin);
+            return RedirectToAction("Index");
+        }
+        public ActionResult RemoveAdmin(int id)
+        {
+            TblAdmins values = repo.Find(x => x.Id == id);
+            repo.TRemove(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateAdmin(int id)
+        {
+            TblAdmins values = repo.Find(x => x.Id == id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult UpdateAdmin(TblAdmins entity)
+        {
+            TblAdmins values = repo.Find(x => x.Id == entity.Id);
+            values.UserName = entity.UserName;
+            values.Password = entity.Password;
+            repo.TUpdate(values);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MvcCv/Controllers/LoginController.cs
+++ MvcCv/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -34,6 +35,12 @@ namespace MvcCv.Controllers
                 TempData["LoginError"] = "Kullanıcı adı veya şifre hatalı, lütfen tekrar deneyin.";
                 return RedirectToAction("Index", "Login");
             }
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
         }
     }
 }
